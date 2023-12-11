8ee6f1f31f2f05c98fa885a8db1d63dc1274c68f
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2023-12-11 11:36:56 +0100

    start work on templating

---
+++ docs/modules/meluxina/pages/render.py
@@ -0,0 +1,42 @@
+import json
+import glob
+from jinja2 import Environment, FileSystemLoader
+
+# Setup Jinja2 environment
+env = Environment(loader=FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
+
+# Path to the JSON files
+json_files_path = 'kub/scenario0/*.json'
+
+
+def convert_hostname(hostname):
+    if hostname.startswith("mel"):
+        return "meluxina"
+    # You can add more rules here if needed
+    return hostname
+
+
+# Iterate over each JSON file in the directory
+for json_file in glob.glob(json_files_path):
+    # Load JSON data
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    # Add the custom filter to the Jinja2 environment
+    env.filters['convert_hostname'] = convert_hostname
+
+    # Load the Jinja2 template
+    template = env.get_template('template.adoc.j2')
+
+    data['filename'] = json_file
+
+    # Render the template with data
+    output = template.render(data)
+
+    # Define the output file name (based on the JSON file name)
+    output_file_name = 'output_' + \
+        json_file.split('/')[-1].replace('.json', '.adoc')
+
+    # Write the output to a file
+    with open(output_file_name, 'w') as file:
+        file.write(output)
