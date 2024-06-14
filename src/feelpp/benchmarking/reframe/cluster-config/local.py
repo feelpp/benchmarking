992c1906cf6243093ac072ee533c898230ff66ae
Tanguy Pierre <tanguy.pierre97@gmail.com>
2024-06-14 14:34:44 +0200

    Values extraction #53 addition of local pc config

---
+++ src/feelpp/benchmarking/reframe/cluster-config/local.py
@@ -0,0 +1,36 @@
+site_configuration = {
+    'systems': [
+        {
+            'name': 'local',
+            'descr': 'Local desktop with 8 cpus',
+            'hostnames': ['local'],
+            'modules_system': 'nomod',
+            'partitions': [
+                {
+                    'name': 'default',
+                    'scheduler': 'local',
+                    'launcher': 'mpiexec',
+                    'environs': ['env_local'],
+                    'processor': {
+                        'num_cpus': 8
+                    },
+                    'devices': [
+                        {
+                            'name': 'cpu-node',
+                            'num_devices': 1
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    'environments': [
+        {
+            'name': 'env_local',
+            'cc': 'gcc',
+            'cxx': 'g++',
+            'ftn': 'gfortran',
+            'target_systems': ['local:default']
+        }
+    ]
+}
