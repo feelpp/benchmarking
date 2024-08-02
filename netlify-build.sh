acb09f65cc7faf8172544a275e45cc5f2e15b114
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2024-08-02 08:34:18 +0200

    update netlify
    #54

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-antora --html-url-extension-style=indexify site.yml
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+npm i
+npx antora --stacktrace generate --cache-dir cache --clean --html-url-extension-style=indexify site.yml
