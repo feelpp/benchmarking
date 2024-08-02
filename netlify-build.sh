609a3c030e525232dcc9a480ed48ff7d15bcef41
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2024-08-02 08:41:16 +0200

    build wheel
    #56

--- netlify-build.sh
+++ netlify-build.sh
@@ -2,6 +2,9 @@
 
 python3 -m venv .venv
 source .venv/bin/activate
+
 pip3 install -r requirements.txt
+pip3 wheel --no-deps --wheel-dir dist .
+pip3 install dist/*.whl
 npm i
 npx antora --stacktrace generate --cache-dir cache --clean --html-url-extension-style=indexify site.yml
