4f5536841b33a5b89200d2f71dcc8d9c8791f3c6
Tanguy Pierre <tanguy.pierre97@gmail.com>
2024-10-01 18:46:17 +0200

    Refactor, document and modularise code #72 parser refraction

--- src/feelpp/benchmarking/feelpp_toolboxes/launchProcess.py
+++ src/feelpp/benchmarking/feelpp_toolboxes/launchProcess.py
@@ -1,81 +1,22 @@
 import os
-import sys
 import json
-import glob
 import shutil
-from jsonschema                         import validate, ValidationError
-from datetime                           import datetime
-from feelpp.benchmarking.feelpp_toolboxes.parser         import parseArgs, printArgs
-from feelpp.benchmarking.feelpp_toolboxes.config.configReader   import supported_env_vars
+from datetime import datetime
 from feelpp.benchmarking.report.handlers import GirderHandler
+from feelpp.benchmarking.feelpp_toolboxes.parser import Parser
+from feelpp.benchmarking.feelpp_toolboxes.config.configReader import supported_env_vars
 
 
 def getParentFolder():
     return os.path.abspath(os.path.dirname(__file__))
 
-
-def buildConfigList(args):
-    configs = []
-
-    if args.dir:
-        for dir in args.dir:
-            path = os.path.join(dir, '**/*.json')
-            json_files = glob.glob(path, recursive=True)
-
-            for file in json_files:
-                basename = os.path.basename(file)
-                if args.exclude and basename in args.exclude:
-                    continue
-                if args.config and basename not in args.config:
-                    continue
-                configs.append(file)
-
-    elif args.config:
-        configs = args.config
-
-    return [os.path.abspath(config) for config in configs]
-
-
-def validateConfigs(configs):
-    parent_folder = getParentFolder()
-    schema_path = f'{parent_folder}/config/configSchema.json'
-
-    with open(schema_path, 'r') as file:
-        schema = json.load(file)
-
-    unvalid = []
-    messages = []
-    instance = None
-    for config in configs:
-        try:
-            with open(config, 'r') as file:
-                instance = json.load(file)
-        except json.JSONDecodeError as e:
-            unvalid.append(config)
-            messages.append(f"Invalid JSON format: {e.msg}")
-
-        if instance:
-            try:
-                validate(instance=instance, schema=schema)
-            except ValidationError as e:
-                unvalid.append(config)
-                messages.append(f"Validation error: {e.message}")
-
-    if unvalid:
-        print("\n[Error] Corrupted configuration files. Please check before relaunch or use --exclude option.")
-        for i in range(len(unvalid)):
-            print(f"> file {i+1}:", unvalid[i])
-            print(f"\t  {messages[i]}")
-        sys.exit(1)
-
-
-def globalVarExporter(hostname, feelppdbPath):
+def globalVarExporter(hostname, feelppdb_path, policy):
     os.environ['WORKDIR'] = getParentFolder()
     os.environ['HOSTNAME'] = hostname
-    os.environ['FEELPPDB_PATH'] = feelppdbPath
+    os.environ['FEELPPDB_PATH'] = feelppdb_path
+    os.environ['EXEC_POLICY'] = policy
 
-
-def buildReportPath(configPath, prefix, suffix, toolbox, hostname, format="%Y%m%dT%H%M%S"):
+def buildReportPath(config_path, prefix, suffix, toolbox, hostname, format="%Y%m%dT%H%M%S"):
     date = datetime.now().strftime(format)
     parent_folder = getParentFolder()
 
@@ -83,34 +24,31 @@ def buildReportPath(configPath, prefix, suffix, toolbox, hostname, format="%Y%m%
         prefix = f'{parent_folder}/toolboxes_reframe_reports/{toolbox}/{hostname}/'
 
     if suffix == '' or suffix == 'default':
-        configName = os.path.basename(configPath)
+        configName = os.path.basename(config_path)
         suffix = f'{date}-{configName}'
 
     reportPath = prefix + suffix
     return reportPath
 
-
-def buildMapToExport(configPath, hostname):
+def buildMapToExport(config_path, hostname):
     varMap = {}
-    varMap['CONFIG_PATH'] = configPath
+    varMap['CONFIG_PATH'] = config_path
 
-    with open(configPath, 'r') as file:
+    with open(config_path, 'r') as file:
         data = json.load(file)
 
     toolbox = data['feelpp']['toolbox']
+    prefix = data['reframe']['report_prefix'].strip()
+    suffix = data['reframe']['report_suffix'].strip()
+
     varMap['TOOLBOX'] = toolbox
     varMap['RFM_PREFIX'] = data['reframe']['directories']['prefix']
     varMap['RFM_STAGE_DIR'] = data['reframe']['directories']['stage']
     varMap['RFM_OUTPUT_DIR'] = data['reframe']['directories']['output']
-
-    prefix = data['reframe']['report_prefix'].strip()
-    suffix = data['reframe']['report_suffix'].strip()
-    varMap['RFM_REPORT_FILE'] = buildReportPath(configPath, prefix, suffix, toolbox, hostname)
-
+    varMap['RFM_REPORT_FILE'] = buildReportPath(config_path, prefix, suffix, toolbox, hostname)
     varMap['GIRDER_FOLDER_ID'] = data['reframe']['directories']['girder_folder_id']
 
     return varMap
-
 
 def processEnvVars(value):
     for var in supported_env_vars:
@@ -118,54 +56,45 @@ def processEnvVars(value):
             value = value.replace(f'${{{var}}}', os.environ[f'{var}'])
     return value
 
-
-def configVarExporter(configPath, hostname):
-    for key, value in buildMapToExport(configPath, hostname).items():
+def configVarExporter(config_path, hostname):
+    for key, value in buildMapToExport(config_path, hostname).items():
         value = value.strip()
         if value != '':
             os.environ[key] = processEnvVars(value)
 
+def buildReframeCommands(args):
+    parent_folder = getParentFolder()
+    cmd = [
+        "reframe"
+        f"-C {parent_folder}/reframe/config-files/{args.hostname}.py"
+        f"-c {parent_folder}/reframe/regression-tests/cpuVariation.py",
+        f"--system={args.hostname}",
+        f"--exec-policy={args.policy}" #async/serial
+    ]
+
+    cmd += ['-l'] if args.list else ['-r']
+    if args.verbose:
+        cmd += ['-' + 'v' * args.verbose]
+    return cmd
+
 
 def launchReframe():
-    parent_folder = getParentFolder()
-    args = parseArgs()
-    printArgs(args)
+    parser = Parser()
+    parser.printArgs()
 
-    configs = buildConfigList(args)
-    validateConfigs(configs)
+    globalVarExporter(parser.args.hostname, parser.args.feelppdb, parser.args.policy)
+    for config in parser.args.config:
 
-    if args.list_files:
-        print("\nFollowing configuration files have been found and validated:")
-        for configPath in configs:
-            print(f"\t> {configPath}")
-        print(f"\nTotal: {len(configs)} file(s)")
+        configVarExporter(config, parser.args.hostname)
+        print(f"[{os.environ['TOOLBOX'].upper()} - {os.path.basename(config)}]")
 
-    else:
-        globalVarExporter(args.hostname, args.feelppdb)
-        for configPath in configs:
+        cmd = buildReframeCommands(parser.args)
+        os.system(cmd)
 
-            configVarExporter(configPath, args.hostname)
-            print(f"[{os.environ['TOOLBOX'].upper()} - {os.path.basename(configPath)}]")
+        #============ UPLOAD REPORTS TO GIRDER ================#
+        girder_handler = GirderHandler(download_base_dir=None)
+        girder_handler.uploadFileToFolder(os.environ['RFM_REPORT_FILE'],os.environ['GIRDER_FOLDER_ID'])
+        #======================================================#
 
-            os.environ["EXEC_POLICY"] = args.policy
-
-            # --report-file option replaced in favour of 'RFM_REPORT_FILE' environment variable
-            cmd = [ f'-C {parent_folder}/reframe/config-files/{args.hostname}.py',
-                    f'-c {parent_folder}/reframe/regression-tests/cpuVariation.py',
-                    f'--system={args.hostname}',
-                    f'--exec-policy={args.policy}']    #async/serial
-
-            cmd += ['-l'] if args.list else ['-r']
-            if args.verbose:
-                cmd += ['-' + 'v' * args.verbose]
-
-            os.system(' '.join(['reframe'] + cmd))
-
-            #============ UPLOAD REPORTS TO GIRDER ================#
-            girder_handler = GirderHandler(download_base_dir=None)
-            girder_handler.uploadFileToFolder(os.environ['RFM_REPORT_FILE'],os.environ['GIRDER_FOLDER_ID'])
-            #======================================================#
-
-
-            print("\n" + '=' * shutil.get_terminal_size().columns)
-        print("\n >>> Number of tests run:", len(configs))
+        print("\n" + '=' * shutil.get_terminal_size().columns)
+    print("\n >>> Number of tests run:", len(parser.args.config))
--- src/feelpp/benchmarking/feelpp_toolboxes/parser.py
+++ src/feelpp/benchmarking/feelpp_toolboxes/parser.py
@@ -1,11 +1,10 @@
 import os
 import sys
+import glob
 import shutil
+import json
+from jsonschema import validate, ValidationError
 from argparse import ArgumentParser, RawTextHelpFormatter
-
-
-# Reframe host config-file missing for:     'discoverer', 'karolina', 'meluxina'
-validHostnames = ['gaya', 'local', 'discoverer', 'karolina', 'meluxina']
 
 
 class CustomHelpFormatter(RawTextHelpFormatter):
@@ -43,6 +42,7 @@ class CustomHelpFormatter(RawTextHelpFormatter):
                     usageArgs.append(f"[{action.option_strings[0]} {action.metavar}]")
                 else:
                     usageArgs.append(f"[{action.option_strings[0]}]")
+
             # Positional arguments
             else:
                 usageArgs.append(f"{action.dest.upper()}")
@@ -50,98 +50,124 @@ class CustomHelpFormatter(RawTextHelpFormatter):
         return usage + ' '.join(usageArgs) + "\n"
 
 
+class Parser():
 
-def parseArgs():
+    def __init__(self):
+        self.parser = ArgumentParser(formatter_class=CustomHelpFormatter, add_help=False)
+        self.valid_hostnames = ['gaya', 'local', 'discoverer', 'karolina', 'meluxina']
+        self.addArgs()
+        self.args = self.parser.parse_args()
+        self.processArgs()
+        if self.args.list_files:
+            self.listFilesAndExit()
 
-    parser = ArgumentParser(formatter_class=CustomHelpFormatter, add_help=False)
+    def processArgs(self):
+        self.validateOptions()
+        if self.args.dir:
+            self.checkDirectoriesExist()
+        self.buildConfigList()
+        self.validateConfigs()
 
-    positional = parser.add_argument_group("Positional arguments")
-    positional.add_argument('hostname', type=str, choices=validHostnames, help='Name of the machine \nValid choices: {%(choices)s}', metavar='hostname')
+    def addArgs(self):
+        positional = self.parser.add_argument_group("Positional arguments")
+        positional.add_argument('hostname', type=str, choices=self.valid_hostnames, help='Name of the machine \nValid choices: {%(choices)s}', metavar='hostname')
 
-    options = parser.add_argument_group("Options")
-    options.add_argument('--feelppdb', '-f', type=str, required=True, metavar='PATH', help='Path to feelppdb folder (required)')
-    #options.add_argument('--report-prefix', '-r', type=str, metavar='REPORT', help='Prefix for Reframe\'s run-report path \nSuffixe is always: \'{date}-{configBasename}.json\'')
-    options.add_argument('--config', '-c', type=str, nargs='+', action='extend', default=[], metavar='CONFIG', help='Paths to JSON configuration files \nIn combination with --dir, specify only basenames for selecting JSON files')
-    options.add_argument('--dir', '-d', type=str, nargs='+', action='extend', default=[], metavar='DIR', help='Name of the directory containing JSON configuration files')
-    options.add_argument('--exclude', '-e', type=str, nargs='+', action='extend', default=[], metavar='EXCLUDE', help='To use in combination with --dir, mentioned files will not be launched')
-    options.add_argument('--policy', '-p', type=str, choices=['async', 'serial'], default='serial', metavar='POLICY', help='Reframe\'s execution policy: {%(choices)s} (default: serial)')
-    options.add_argument('--list', '-l', action='store_true', help='List all parametrized tests that will be run by Reframe')
-    options.add_argument('--list-files', '-lf', action='store_true', help='List all benchmarking configuration file found')
-    options.add_argument('--verbose', '-v', action='count', default=0, help='Select Reframe\'s verbose level by specifying multiple v\'s')
-    options.add_argument('--help', '-h', action='help', help='Display help and quit program')
+        options = self.parser.add_argument_group("Options")
+        options.add_argument('--feelppdb', '-f', type=str, required=True, metavar='PATH', help='Path to feelppdb folder (required)')
+        #options.add_argument('--report-prefix', '-r', type=str, nargs='+', metavar='REPORT', help='Prefix for Reframe\'s run-report path \nSuffixe is always: \'{date}-{configBasename}.json\'')
+        options.add_argument('--config', '-c', type=str, nargs='+', action='extend', default=[], metavar='CONFIG', help='Paths to JSON configuration files \nIn combination with --dir, specify only basenames for selecting JSON files')
+        options.add_argument('--dir', '-d', type=str, nargs='+', action='extend', default=[], metavar='DIR', help='Name of the directory containing JSON configuration files')
+        options.add_argument('--exclude', '-e', type=str, nargs='+', action='extend', default=[], metavar='EXCLUDE', help='To use in combination with --dir, mentioned files will not be launched')
+        options.add_argument('--policy', '-p', type=str, choices=['async', 'serial'], default='serial', metavar='POLICY', help='Reframe\'s execution policy: {%(choices)s} (default: serial)')
+        options.add_argument('--list', '-l', action='store_true', help='List all parametrized tests that will be run by Reframe')
+        options.add_argument('--list-files', '-lf', action='store_true', help='List all benchmarking configuration file found')
+        options.add_argument('--verbose', '-v', action='count', default=0, help='Select Reframe\'s verbose level by specifying multiple v\'s')
+        options.add_argument('--help', '-h', action='help', help='Display help and quit program')
 
-    args = parser.parse_args()
+    def validateOptions(self):
+        if not self.args.config and not self.args.dir:
+            print(f'[Error] At least one of --config or --dir option must be specified')
+            sys.exit(1)
 
-    args.config = handleColonSeparator(args.config)
-    args.dir = handleColonSeparator(args.dir)
-    args.exclude = handleColonSeparator(args.exclude)
+        if self.args.config and len(self.args.dir) > 1:
+            print(f'[Error] --dir and --config combination can only handle one DIR')
+            sys.exit(1)
 
-    argsValidation(args)
-
-    return args
-
-
-# not really useful as arguments can be passed multiple times with blank space or multiple -c
-def handleColonSeparator(arg):
-    splitted = []
-    if isinstance(arg, list):
-        for elem in arg:
-            if ':' in elem:
-                splitted.extend(elem.split(':'))
-            else:
-                splitted.append(elem)
-        arg = splitted
-
-    elif isinstance(arg, str):
-        if ':' in arg:
-            splitted.append(arg.split(':'))
-    return arg
-
-
-def printListAndExit(lst, name):
-    for i,elem in enumerate(lst):
-        print(f" > {name} {i+1}:", elem)
-    sys.exit(1)
-
-
-def argsValidation(args):
-    if not args.config and not args.dir:
-        print(f'[Error] At least one of --config or --dir option must be specified')
-        sys.exit(1)
-
-    if args.config and len(args.dir) > 1:
-        print(f'[Error] --dir and --config combination can only handle one DIR')
-        sys.exit(1)
-
-    notFound = []
-    if args.config:
-        if not args.dir:
-            for config in args.config:
-                if not os.path.exists(config):
-                    notFound.append(config)
-        else:
-            # args.dir is a list with 1 element
-            dir = args.dir[0]
-            for config in args.config:
-                path = os.path.join(dir, config)
-                if not os.path.exists(path):
-                    notFound.append(config)
-
-        if notFound:
-            print(f'[Error] Following configuration files were not found')
-            printListAndExit(notFound, 'file')
-
-    elif args.dir:
-        for dir in args.dir:
+    def checkDirectoriesExist(self):
+        not_found = []
+        for dir in self.args.dir:
             if not os.path.isdir(dir):
-                notFound.append(dir)
-        if notFound:
+                not_found.append(dir)
+
+        if not_found:
             print(f'[Error] Following directories were not found')
-            printListAndExit(notFound, 'dir')
+            for dir in not_found:
+                print(f" > {dir}")
+            sys.exit(1)
+
+    def buildConfigList(self):
+        configs = []
+        if self.args.dir:
+            for dir in self.args.dir:
+                path = os.path.join(dir, '**/*.json')
+                json_files = glob.glob(path, recursive=True)
+                configs.extend(json_files)
+            if self.args.config:
+                configs = [config for config in configs if os.path.basename(config) in self.args.config]
+
+        if self.args.config and not self.args.dir:
+            configs = self.args.config
+
+        if self.args.exclude:
+            configs = [config for config in configs if os.path.basename(config) not in self.args.exclude]
+
+        self.args.config = [os.path.abspath(config) for config in configs]
 
 
-def printArgs(args):
-    print("\n[Loaded command-line options]")
-    for arg in vars(args):
-        print(f"\t > {arg + ':' :<{20}} {getattr(args, arg)}")
-    print("\n" + '=' * shutil.get_terminal_size().columns)
+    def validateConfigs(self):
+        parent_folder = os.path.abspath(os.path.dirname(__file__))
+        schema_path = f'{parent_folder}/config/configSchema.json'
+
+        with open(schema_path, 'r') as file:
+            schema = json.load(file)
+
+        unvalid = []
+        messages = []
+        for config in self.args.config:
+            instance = None
+            try:
+                with open(config, 'r') as file:
+                    instance = json.load(file)
+            except FileNotFoundError as e:
+                unvalid.append(config)
+                messages.append(f"File not found: {e.strerror}")
+            except json.JSONDecodeError as e:
+                unvalid.append(config)
+                messages.append(f"Invalid JSON format: {e.msg}")
+
+            if instance:
+                try:
+                    validate(instance=instance, schema=schema)
+                except ValidationError as e:
+                    unvalid.append(config)
+                    messages.append(f"Validation error: {e.message}")
+
+        if unvalid:
+            print("\n[Error] Corrupted configuration files. Please check before relaunch or use --exclude option.")
+            for i in range(len(unvalid)):
+                print(f"> file {i+1}:", unvalid[i])
+                print(f"\t  {messages[i]}")
+            sys.exit(1)
+
+    def listFilesAndExit(self):
+        print("\nFollowing configuration files have been found and validated:")
+        for config_path in self.args.config:
+            print(f"\t> {config_path}")
+        print(f"\nTotal: {len(self.args.config)} file(s)")
+        sys.exit(0)
+
+    def printArgs(self):
+        print("\n[Loaded command-line options]")
+        for arg in vars(self.args):
+            print(f"\t > {arg + ':' :<{20}} {getattr(self.args, arg)}")
+        print("\n" + '=' * shutil.get_terminal_size().columns)
