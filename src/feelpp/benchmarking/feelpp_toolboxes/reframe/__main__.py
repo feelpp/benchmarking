ded3066a24569a260278700cfa9c42343781b8dd
Javier Cladellas <javiercladellas@gmail.com>
2024-10-02 14:35:31 +0200

    factor reframe cmd

--- src/feelpp/benchmarking/feelpp_toolboxes/reframe/__main__.py
+++ src/feelpp/benchmarking/feelpp_toolboxes/reframe/__main__.py
@@ -3,11 +3,32 @@ from feelpp.benchmarking.feelpp_toolboxes.parser import Parser
 from feelpp.benchmarking.feelpp_toolboxes.config.configReader import ConfigReader
 from feelpp.benchmarking.feelpp_toolboxes.config.configSchema import MachineConfig
 
+
+class CommandBuilder:
+    def __init__(self, machine_config, parser):
+        self.machine_config = machine_config
+        self.parser = parser
+
+    def build_command(self):
+        cmd = [
+            'reframe',
+            f'-C {self.machine_config.config_file}',
+            f'-c ./src/feelpp/benchmarking/feelpp_toolboxes/reframe/regression.py',
+            f'--system={self.machine_config.hostname}',
+            f'--exec-policy={self.machine_config.execution_policy}',
+            '-r',
+            f'{"-"+"v"*self.parser.args.verbose  if self.parser.args.verbose else ""}'
+        ]
+        return ' '.join(cmd)
+
+
 if __name__ == "__main__":
 	parser = Parser()
 	parser.printArgs()
 
 	machine_config = ConfigReader(parser.args.exec_config,MachineConfig).config
+
+	cmd_builder = CommandBuilder(machine_config,parser)
 
 	os.environ['RFM_STAGE_DIR'] = machine_config.reframe_stage
 	os.environ['RFM_OUTPUT_DIR'] = machine_config.reframe_output
@@ -17,13 +38,6 @@ if __name__ == "__main__":
 	for config_filepath in parser.args.config:
 		os.environ["JSON_CONFIG_PATH"] = config_filepath
 
-		cmd = [
-			'reframe',
-			f'-C {machine_config.config_file}',
-			f'-c ./src/feelpp/benchmarking/feelpp_toolboxes/reframe/regression-tests/regression.py',
-			f'--system={machine_config.hostname}',
-			f'--exec-policy={machine_config.execution_policy}',
-			'-r',
-			f'{"-"+"v"*parser.args.verbose  if parser.args.verbose else ""}'
-		]
-		os.system(' '.join(cmd))
+		reframe_cmd = cmd_builder.build_command()
+
+		os.system(reframe_cmd)
