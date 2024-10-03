6f50fc9e7f7ec136bffff92731435848acd84c92
Javier Cladellas <javiercladellas@gmail.com>
2024-10-03 17:04:26 +0200

    add perfvars

--- src/feelpp/benchmarking/feelpp_toolboxes/reframe/regression.py
+++ src/feelpp/benchmarking/feelpp_toolboxes/reframe/regression.py
@@ -4,6 +4,11 @@ from feelpp.benchmarking.feelpp_toolboxes.reframe.setup import ReframeSetup
 @rfm.simple_test
 class RegressionTest(ReframeSetup):
 
+
+    @run_before('performance')
+    def setPerfVars(self):
+        self.perf_variables = self.scalability_handler.getPerformanceVariables(self.nb_tasks)
+
     @sanity_function
     def sanityCheck(self):
         return (
