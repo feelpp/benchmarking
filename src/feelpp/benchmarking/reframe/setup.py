c64015e0067f7f9c36aeb565dd8531a28cf291dd
Javier Cladellas <javiercladellas@gmail.com>
2024-10-07 14:11:34 +0200

    fix hsize param and save with hashcode #75

--- src/feelpp/benchmarking/reframe/setup.py
+++ src/feelpp/benchmarking/reframe/setup.py
@@ -2,8 +2,6 @@
 from feelpp.benchmarking.reframe.parameters import NbTasks,Discretization
 from feelpp.benchmarking.reframe.config.configReader import ConfigReader
 from feelpp.benchmarking.reframe.config.configSchemas import ConfigFile,MachineConfig
-from feelpp.benchmarking.reframe.validation import ValidationHandler
-from feelpp.benchmarking.reframe.scalability import ScalabilityHandler
 
 import reframe as rfm
 import os, re
@@ -94,7 +92,13 @@ class AppSetup(Setup):
             config_filepath (str): Path of the application configuration json file
         """
         super().__init__()
+        self.config_filepath = config_filepath
         self.config = ConfigReader(config_filepath,ConfigFile).config
+
+        self.template_pattern = re.compile(r"\{\{(.*?)\}\}")
+
+    def reset(self):
+        self.__init__(self.config_filepath)
 
     def setupBeforeRun(self,rfm_test):
         """ Methods to be executed before run step of a reframe test
@@ -111,17 +115,40 @@ class AppSetup(Setup):
         rfm_test.executable = self.config.executable
         rfm_test.executable_opts = self.config.options
 
-    def updateOptions(self,replace):
+    def replaceField(self,field, replace):
+        """ Replaces a single string {{stored.like.this}} with their actual replace value
+        Args:
+            field (str): The string to find the template
+            replace (str): The value to replace with
+        """
+        return self.template_pattern.sub(lambda match: replace.get(match.group(1).strip(), match.group(0)), field)
+
+    def updateDict(self, obj, replace):
         """ Replaces template values {{stored.like.this}} with their actual values
         Args:
+            obj (dict): The dictionary containing the templates
             replace (dict): key,value pairs representing the placeholder name and the actual value to replace with
         """
+        new_cfg = {}
 
-        # Update options using a list comprehension
-        self.config.options = [
-            re.compile(r"\{\{(.*?)\}\}").sub(lambda match: replace.get(match.group(1).strip(), match.group(0)), option)
-            for option in self.config.options
-        ]
+        for field, value in obj.items():
+            if isinstance(value, dict):
+                new_cfg[field] = self.updateDict(value, replace)
+            elif isinstance(value, list):
+                new_cfg[field] = [self.replaceField(v,replace) if isinstance(v, str) else v for v in value ]
+            elif isinstance(value, str):
+                new_cfg[field] = self.replaceField(value,replace)
+            else:
+                new_cfg[field] = value
+
+        return new_cfg
+
+    def updateConfig(self, replace):
+        """ Replace the template values on the config attribute with variable values
+        Args:
+            replace (dict[str,str]): key,value pairs representing the placeholder name and the actual value to replace with
+        """
+        self.config = ConfigFile(** self.updateDict(self.config.model_dump(),replace))
 
 @rfm.simple_test
 class ReframeSetup(rfm.RunOnlyRegressionTest):
@@ -145,8 +172,6 @@ class ReframeSetup(rfm.RunOnlyRegressionTest):
             case _:
                 raise NotImplementedError
 
-    validation_handler = ValidationHandler(app_setup.config.sanity)
-    scalability_handler = ScalabilityHandler(app_setup.config.scalability)
 
     @run_after('init')
     def initSetups(self):
@@ -158,7 +183,11 @@ class ReframeSetup(rfm.RunOnlyRegressionTest):
         """ Sets the necessary post-init configurations"""
         self.machine_setup.setupAfterInit(self)
         self.app_setup.setupAfterInit(self)
-        self.scalability_handler.cleanupScalabilityFiles()
+
+    @run_before('run')
+    def updateSetups(self):
+        """Updates the setup with testcase related values"""
+        self.app_setup.reset()
 
     @run_before('run')
     def setupParameters(self):
@@ -168,9 +197,9 @@ class ReframeSetup(rfm.RunOnlyRegressionTest):
             self.num_cpus_per_task = 1
             self.num_tasks = self.nb_tasks
         if hasattr(self, "hsize"):
-            self.app_setup.updateOptions({
-                "parameters.discretization.value":str(self.hsize)
-            })
+            self.app_setup.updateConfig({ "parameters.discretization.value":str(self.hsize) })
+
+        self.app_setup.updateConfig({ "instance": str(self.hashcode) })
 
 
     @run_before('run')
