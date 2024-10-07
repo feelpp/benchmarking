ef5ecfab2354a10b5e61c7cd7cd607f75269a2a2
Javier Cladellas <javiercladellas@gmail.com>
2024-10-07 14:11:10 +0200

    refactor #75

--- src/feelpp/benchmarking/reframe/regression.py
+++ src/feelpp/benchmarking/reframe/regression.py
@@ -1,11 +1,17 @@
 import reframe as rfm
 from feelpp.benchmarking.reframe.setup import ReframeSetup
+from feelpp.benchmarking.reframe.validation import ValidationHandler
+from feelpp.benchmarking.reframe.scalability import ScalabilityHandler
 
 @rfm.simple_test
 class RegressionTest(ReframeSetup):
     """ Class to execute reframe test.
     It should contain sanity functions and performance variable setting"""
 
+    @run_before('run')
+    def initHandlers(self):
+        self.validation_handler = ValidationHandler(self.app_setup.config.sanity)
+        self.scalability_handler = ScalabilityHandler(self.app_setup.config.scalability)
 
     @run_before('performance')
     def setPerfVars(self):
