648b59c3ca78b68c893d1ce709b2287eac24b11c
Javier Cladellas <javiercladellas@gmail.com>
2024-10-11 16:15:47 +0200

    add machine MVC #77

--- src/feelpp/benchmarking/report/__main__.py
+++ src/feelpp/benchmarking/report/__main__.py
@@ -22,43 +22,22 @@ def main_cli():
     applications = ApplicationRepository(config_handler.applications)
     use_cases = UseCaseRepository(config_handler.use_cases)
     machines = MachineRepository(config_handler.machines)
-
-    atomic_reports = AtomicReportRepository(
-        benchmarking_config_json = config_handler.execution_mapping,
-        download_handler = girder_handler,
-    )
+    atomic_reports = AtomicReportRepository(config_handler.execution_mapping,girder_handler)
 
     machines.link(applications, use_cases, config_handler.execution_mapping)
     applications.link(machines, use_cases, config_handler.execution_mapping)
     use_cases.link(applications, machines, config_handler.execution_mapping)
-
     atomic_reports.link(applications, machines, use_cases)
 
-
-    machines.printHierarchy()
-    print("-----------------")
-    applications.printHierarchy()
-    print("-----------------")
-    use_cases.printHierarchy()
-
-
     index_renderer = Renderer("./src/feelpp/benchmarking/report/templates/index.adoc.j2")
+    machine_report_renderer = Renderer("./src/feelpp/benchmarking/report/templates/machine.adoc.j2")
+    report_renderer = Renderer("./src/feelpp/benchmarking/report/templates/benchmark.adoc.j2")
 
     applications_base_dir = os.path.join(args.modules_path,"applications")
     applications.initModules(applications_base_dir, index_renderer, parent_id="catalog-index")
 
-    report_renderer = Renderer("./src/feelpp/benchmarking/report/templates/benchmark.adoc.j2")
+    for machine in machines:
+        machine.createOverview(applications_base_dir,machine_report_renderer)
 
-    counter = {
-        f"{app.id}-{mach.id}-{tc.id}" : 0
-        for app in applications
-        for tc in app.tree
-        for mach in app.tree[tc]
-    }
-
-    #TODO: At the moment just generate 5 reports per test case
     for atomic_report in atomic_reports:
-        if counter[f"{atomic_report.application_id}-{atomic_report.machine_id}-{atomic_report.use_case_id}"] < 5:
-            atomic_report.createReport(applications_base_dir,report_renderer)
-
-        counter[f"{atomic_report.application_id}-{atomic_report.machine_id}-{atomic_report.use_case_id}"] += 1
+        atomic_report.createReport(applications_base_dir,report_renderer)
--- src/feelpp/benchmarking/report/components/machine.py
+++ src/feelpp/benchmarking/report/components/machine.py
@@ -1,5 +1,8 @@
 import os
+import pandas as pd
 from feelpp.benchmarking.report.components.baseComponent import BaseComponent
+from feelpp.benchmarking.report.components.figureFactory import FigureFactory
+from feelpp.benchmarking.reframe.config.configSchemas import Plot
 
 class Machine(BaseComponent):
     """ Class representing a machine module/component.
@@ -14,3 +17,118 @@ class Machine(BaseComponent):
             description (str): The description of the machine
         """
         super().__init__(id, display_name, description)
+
+        self.plots_config = [
+            {
+                "title": "Application performance over time",
+                "plot_types": [
+                    "scatter"
+                ],
+                "transformation": "performance_sum",
+                "variables": [
+                    "ElectricConstructor_init",
+                    "ElectricPostProcessing_exportResults",
+                    "ElectricSolve_solve"
+                ],
+                "names": [
+                    "Application Performance"
+                ],
+                "xaxis": {
+                    "parameter": "time_start",
+                    "label": "Date"
+                },
+                "secondary_axis": {
+                    "parameter": "hsize",
+                    "label": "h size"
+                },
+                "yaxis": {
+                    "label": "Execution time (s)"
+                },
+                "color_axis":{
+                    "parameter": "nb_tasks",
+                    "label": "Tasks"
+                }
+            }
+        ]
+
+    def createOverview(self, base_dir, renderer):
+        """ Create the overview for an app-machine-usecase combination, from aggregating atomic report data
+        Args:
+            base_dir (str): The base directory where the report will be created
+            renderer (Renderer): The renderer to use
+        """
+        for app, use_cases in self.tree.items():
+            for use_case, reports in use_cases.items():
+
+                output_folder_path = f"{base_dir}/{app.id}/{use_case.id}/{self.id}"
+
+                if not os.path.exists(output_folder_path):
+                    raise FileNotFoundError(f"The folder {output_folder_path} does not exist. Modules should be initialized beforehand ")
+
+                renderer.render(
+                    f"{output_folder_path}/overview.adoc",
+                    dict(
+                        reports_dfs = { report.date: report.model.master_df.to_dict(orient='dict') for report in reports },
+                        plots_config = self.plots_config,
+                        parent_catalogs = f"{app.id}-{use_case.id}-{self.id}",
+                        application_display_name = app.display_name,
+                        machine_display_name = self.display_name,
+                        use_case_display_name = use_case.display_name
+                    )
+                )
+
+
+class MachineModel:
+    """ Model component of the useCase.
+        This class holds the aggregated data for all atomic reports in a use case.
+    """
+    def __init__(self, atomic_models_dfs):
+        self.master_df = self.buildMasterDf( atomic_models_dfs )
+
+    def buildMasterDf(self,atomic_models_dfs):
+        """ creates a dataframe holding all master dataframes from atomic reports.
+            Facilitates pivoting and aggregation
+        Args:
+            atomic_models_dfs (dict[pd.DataFrame]). Dict with atomic report master dataframes (serialized with to_dict(orient='dict')), keys are report's start and end times as tuple
+        """
+        parsed_dfs = []
+        for date, df in atomic_models_dfs.items():
+            parsed_df = pd.DataFrame.from_records(df)
+            parsed_df["time_start"] = date
+            parsed_dfs.append(parsed_df)
+
+        return pd.concat(parsed_dfs ,axis=0)
+
+
+
+class MachineController:
+    """ Controller component of the Machine component, it orchestrates the model with the view"""
+    def __init__(self, model, view):
+        """
+        Args:
+            model (MachineModel): The use case model component
+            view (MachineView): The use case view component
+        """
+        self.model = model
+        self.view = view
+
+    def generateAll(self):
+        """ Creates plotly figures for each plot specified on the view config file
+        Returns a list of plotly figures.
+        """
+        #TODO: Can be a generator
+        figs = []
+        for plot_config in self.view.plots_config:
+            for plot in FigureFactory.create(plot_config):
+                figs.append(plot.createFigure(self.model.master_df))
+        return figs
+
+
+class MachineView:
+    """ View component for the Machine Report, it contains all figure generation related code """
+    def __init__(self,plots_config):
+        """ parses the plots config. This JSON tells what plots to show and how to display them
+        Args:
+            plots_config (dict): Configuration on how to plot the values
+        """
+        self.plots_config = [Plot(**d) for d in plots_config]
