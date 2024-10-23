2f1f1b2f4ab873501a1ae0e7d4beb261effacb79
Javier Cladellas <javiercladellas@gmail.com>
2024-10-23 11:34:49 +0200

    up parameter strategies #97

--- src/feelpp/benchmarking/reframe/parameters.py
+++ src/feelpp/benchmarking/reframe/parameters.py
@@ -9,88 +9,125 @@ class Parameter:
             param_config (Param). object from the pydantic schema representing a parameter
         """
         self.name = param_config.name
+        self.mode = param_config.mode
         self.active = param_config.active
-        self.range = param_config.range
 
     def parametrize(self):
         """ Pure Virtual function to parametrize a parameter"""
         raise NotImplementedError("This is a pure virtual method that should be overriden by inherited classes")
 
 
-class CoresParameter(Parameter):
-    """ Parameter representing the number of tasks """
-    def __init__(self,param_config):
-        super().__init__(param_config)
+# class CoresParameter(Parameter):
+#     """ Parameter representing the number of tasks """
+#     def __init__(self,param_config):
+#         super().__init__(param_config)
 
-    def parametrize(self):
-        """Parametrize the number of tasks, depending on the desiring sequencing/configuration"""
-        if self.range.generator == "double":
-            for part in rt.runtime().system.partitions:
-                nb_task = self.range.min_cores_per_node
-                yield nb_task
-                while (nb_task < part.processor.num_cpus) and (nb_task < self.range.max_cores_per_node):
-                    nb_task <<= 1
-                    yield nb_task
+#     def parametrize(self):
+#         """Parametrize the number of tasks, depending on the desiring sequencing/configuration"""
+#         if self.range.generator == "double":
+#             for part in rt.runtime().system.partitions:
+#                 nb_task = self.range.min_cores_per_node
+#                 yield nb_task
+#                 while (nb_task < part.processor.num_cpus) and (nb_task < self.range._cores_per_node):
+#                     nb_task <<= 1
+#                     yield nb_task
 
-                if not (self.range.min_nodes == 1 and self.range.max_nodes == 1):
-                    if self.range.max_nodes < part.devices[0].num_devices:
-                        nb_nodes = self.range.max_nodes
-                    else:
-                        nb_nodes = part.devices[0].num_devices
-                    for i in range(self.range.min_nodes+1, nb_nodes+1):
-                        nb_task = i * part.processor.num_cpus
-                        yield nb_task
-        else:
-            raise NotImplementedError
+#                 if not (self.range.min_nodes == 1 and self.range.max_nodes == 1):
+#                     if self.range.max_nodes < part.devices[0].num_devices:
+#                         nb_nodes = self.range.max_nodes
+#                     else:
+#                         nb_nodes = part.devices[0].num_devices
+#                     for i in range(self.range.min_nodes+1, nb_nodes+1):
+#                         nb_task = i * part.processor.num_cpus
+#                         yield nb_task
 
-class StepParameter(Parameter):
-    """ Parameter that changes following a step strategy """
+class LinspaceParameter(Parameter):
     def __init__(self, param_config):
         super().__init__(param_config)
+        self.min = param_config.linspace.min
+        self.max = param_config.linspace.max
+        self.n_steps = param_config.linspace.n_steps
 
     def parametrize(self):
-        if self.range.generator == "linear":
-            assert self.range.n_steps is not None, "Number of steps must be specified for the linear generator"
-            step = (self.range.max - self.range.min) / (self.range.n_steps - 1)
-        elif self.range.generator == "constant":
-            assert self.range.step is not None, "The step must be specified for the constant generator"
-            step = self.range.step
-        else:
-            raise NotImplementedError
-
-        current = self.range.min
-        while current <= self.range.max:
-            yield current
-            current += step
+        yield from np.linspace(self.min,self.max,self.n_steps,endpoint=True,dtype=object)
 
 
-class ListParameter(Parameter):
-    """ Parameter that is given as a list """
+class LogspaceParameter(Parameter):
     def __init__(self, param_config):
         super().__init__(param_config)
+        self.min = param_config.linspace.min
+        self.max = param_config.linspace.max
+        self.n_steps = param_config.linspace.n_steps
+        self.base = param_config.linspace.base
 
     def parametrize(self):
-        if self.range.generator == "ordered":
-            treated_list = self.range.sequence
-        elif self.range.generator == "random":
-            raise NotImplementedError
-        else:
-            raise NotImplementedError
+        yield from np.logspace(self.min,self.max,self.n_steps,endpoint=True,base=self.base,dtype=object)
 
-        for current in treated_list:
-            yield current
+class GeometricParameter(Parameter):
+    def __init__(self, param_config):
+        super().__init__(param_config)
+        self.start = param_config.geometric.start
+        self.ratio = param_config.geometric.ratio
+        self.n_steps = param_config.geometric.n_steps
 
+    def parametrize(self):
+        yield from self.start * (self.ratio ** np.arange(self.n_steps,dtype=object))
 
+class RangeParameter(Parameter):
+    def __init__(self, param_config):
+        super().__init__(param_config)
+        self.min = param_config.range.min
+        self.max = param_config.range.max
+        self.step = param_config.range.step
+
+    def parametrize(self):
+        yield from np.arange(start=self.min, stop=self.max+self.step,step=self.step,dtype=object)
+
+class SequenceParameter(Parameter):
+    def __init__(self, param_config):
+        super().__init__(param_config)
+        self.sequence = param_config.sequence
+
+    def parametrize(self):
+        yield from self.sequence
+
+class RepeatParameter(Parameter):
+    def __init__(self, param_config):
+        super().__init__(param_config)
+        self.value = param_config.repeat.value
+        self.count = param_config.repeat.count
+
+    def parametrize(self):
+        yield from [self.value]*self.count
+
+class ZipParameter(Parameter):
+    def __init__(self, param_config):
+        super().__init__(param_config)
+        self.parameter_generators = {
+            subparam_config.name : ParameterFactory.create(subparam_config).parametrize()
+            for subparam_config in param_config.zip
+        }
+
+    def parametrize(self):
+        yield from ({key: value for key, value in zip(self.parameter_generators.keys(), values)} for values in zip(*self.parameter_generators.values()))
 
 class ParameterFactory:
     """ Factory class to create Parameters for reframe tests"""
     @staticmethod
     def create(param_config):
-        if param_config.range.mode == "cores":
-            return CoresParameter(param_config)
-        elif param_config.range.mode ==  "step":
-            return StepParameter(param_config)
-        elif param_config.range.mode ==  "list":
-            return ListParameter(param_config)
+        if param_config.mode == "linspace":
+            return LinspaceParameter(param_config)
+        elif param_config.mode == "range":
+            return RangeParameter(param_config)
+        elif param_config.mode == "logspace":
+            return LogspaceParameter(param_config)
+        elif param_config.mode == "sequence":
+            return SequenceParameter(param_config)
+        elif param_config.mode == "repeat":
+            return RepeatParameter(param_config)
+        elif param_config.mode == "geometric":
+            return GeometricParameter(param_config)
+        elif param_config.mode == "zip":
+            return ZipParameter(param_config)
         else:
             raise ValueError(f"Unkown parameter type {param_config.range.mode}")
