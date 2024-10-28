f9f151a1cc95fba406dd3e15ca87d3c0d91c2789
Javier Cladellas <javiercladellas@gmail.com>
2024-10-28 14:05:59 +0100

    rm dupe pydantic model

--- src/feelpp/benchmarking/reframe/config/configSchemas.py
+++ src/feelpp/benchmarking/reframe/config/configSchemas.py
@@ -38,34 +38,6 @@ class Upload(BaseModel):
     platform:Literal["girder","ckan"]
     folder_id: Union[str,int]
 
-
-class PlotAxis(BaseModel):
-    parameter: Optional[str] = None
-    label:str
-
-class Aggregation(BaseModel):
-    column: str
-    agg: Literal["sum","mean","min","max"]
-class Plot(BaseModel):
-    title:str
-    plot_types:List[Literal["scatter","table","stacked_bar"]]
-    transformation:Literal["performance","relative_performance","speedup"]
-    aggregations:Optional[List[Aggregation]] = None
-    variables:Optional[List[str]] = None
-    names:List[str]
-    xaxis:PlotAxis
-    secondary_axis:Optional[PlotAxis] = None
-    yaxis:PlotAxis
-    color_axis:Optional[PlotAxis] = None
-
-
-    @field_validator("xaxis","secondary_axis", mode="after")
-    def checExecutableInstalled(cls, v):
-        """ Checks that the parameter field is specified for xaxis and secondary_axis field"""
-        if v:
-            assert v.parameter is not None
-        return v
-
 class Image(BaseModel):
     protocol:Optional[Literal["oras","docker","library","local"]] = None
     name:str
