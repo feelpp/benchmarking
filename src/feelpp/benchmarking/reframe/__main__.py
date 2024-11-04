2882991d4ad499a3599e1088b967e6beb05d4926
Javier Cladellas <javiercladellas@gmail.com>
2024-11-04 11:09:07 +0100

    add logs dir

--- src/feelpp/benchmarking/reframe/__main__.py
+++ src/feelpp/benchmarking/reframe/__main__.py
@@ -35,6 +35,7 @@ class CommandBuilder:
             f'--system={self.machine_config.machine}',
             f'--exec-policy={self.machine_config.execution_policy}',
             f'--prefix={self.machine_config.reframe_base_dir}',
+            f'--logdir={os.path.join(self.machine_config.reframe_base_dir,"logs")}',
             f'--report-file={self.buildReportFilePath(executable)}',
             f'{"-"+"v"*self.parser.args.verbose  if self.parser.args.verbose else ""}',
             '-r',
