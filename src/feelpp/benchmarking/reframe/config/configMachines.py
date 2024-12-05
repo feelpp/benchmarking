a7c73ab519a2e12b459b3a8309de06ab6649f6f7
Javier Cladellas <javiercladellas@gmail.com>
2024-12-05 13:22:09 +0100

    compat with 3.8

--- src/feelpp/benchmarking/reframe/config/configMachines.py
+++ src/feelpp/benchmarking/reframe/config/configMachines.py
@@ -6,7 +6,7 @@ class Container(BaseModel):
     cachedir:Optional[str] = None
     tmpdir:Optional[str] = None
     image_base_dir:str
-    options:Optional[list[str]] = []
+    options:Optional[List[str]] = []
 
     @field_validator("cachedir","tmpdir","image_base_dir",mode="before")
     @classmethod
