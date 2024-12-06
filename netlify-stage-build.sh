f65148b95aa2b982102050a66af0076b5dca0053
Javier Cladellas <javiercladellas@gmail.com>
2024-12-06 17:09:23 +0100

    add first staging workflow #181

---
+++ netlify-stage-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+pip3 wheel --no-deps --wheel-dir dist .
+pip3 install dist/*.whl
+npm i
+render-benchmarks --config_file=./tmp/website_config.json
+npx antora --stacktrace generate --cache-dir cache --clean --html-url-extension-style=indexify site.yml
