a6d294dcd9420ab2647d4b9440ca26b7a2d92d7a
Javier Cladellas <javiercladellas@gmail.com>
2024-12-06 17:09:05 +0100

    add update config option #181

--- src/feelpp/benchmarking/scripts/misc.py
+++ src/feelpp/benchmarking/scripts/misc.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import glob, json, copy
+import glob, json, copy, os
 
 def mergeDicts(dict1, dict2):
     """Recursively merges two dictionaries."""
@@ -24,12 +24,23 @@ def jsonConfigMerge_cli():
     parser = ArgumentParser()
     parser.add_argument("--file_pattern", '-fp', required = True, type=str, help="File glob pattern of the json files to merge")
     parser.add_argument("--output_file_path","-o",required=True,type=str,help="Path of the resulting file to be saved to")
+    parser.add_argument("--update_paths", "-u", action="store_true", help="Whether to update the config file paths")
     args = parser.parse_args()
 
     master_config = {}
     for filename in glob.glob(args.file_pattern,recursive=True):
         with open(filename,"r") as f:
             current_config = json.load(f)
+
+        if args.update_paths:
+            file_dirpath = os.path.dirname(os.path.relpath(filename,"."))
+            report_paths = glob.glob(os.path.join(file_dirpath,"**","reframe_report.json"),recursive=True)
+            for report in report_paths:
+                app,use_case,machine = report.split("/")[-5:-2]
+
+                current_config["execution_mapping"][app][machine][use_case]["path"] = os.path.join(file_dirpath,app,use_case,machine)
+                current_config["execution_mapping"][app][machine][use_case]["path"]["platform"] = "local"
+
             master_config = mergeDicts(master_config,current_config)
 
     with open(args.output_file_path,"w") as f:
