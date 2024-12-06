b9b7ff83c9faac8a59fb8e61553afb71888200c5
Javier Cladellas <javiercladellas@gmail.com>
2024-12-06 15:08:54 +0100

    add dir option to girder-download

--- src/feelpp/benchmarking/scripts/girder.py
+++ src/feelpp/benchmarking/scripts/girder.py
@@ -6,12 +6,16 @@ from argparse import ArgumentParser
 def download_cli():
     parser = ArgumentParser()
     parser.add_argument("--girder_id", '-gid', required = True, type=str, help="Girder file id to download.")
+    parser.add_argument("--directory","-d", action='store_true')
     parser.add_argument("--output_dir", '-o', required = False, type=str, help="Path to the directory to download files to.", default="./tmp/")
     parser.add_argument("--filename", '-fn', required = False, type=str, default=None, help="Name to give to the file after downloading ")
     args = parser.parse_args()
 
     girder_hanlder = GirderHandler(args.output_dir)
-    girder_hanlder.downloadFile(args.girder_id,name=args.filename)
+    if args.directory:
+        girder_hanlder.downloadFolder(args.girder_id,output_dir=".")
+    else:
+        girder_hanlder.downloadFile(args.girder_id,name=args.filename)
 
     return 0
 
