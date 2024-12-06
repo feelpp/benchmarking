440e3f8d75c5adffda6ed6d30799010351ee2cd5
Javier Cladellas <javiercladellas@gmail.com>
2024-12-06 15:29:03 +0100

    help

--- src/feelpp/benchmarking/scripts/girder.py
+++ src/feelpp/benchmarking/scripts/girder.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 def download_cli():
     parser = ArgumentParser()
     parser.add_argument("--girder_id", '-gid', required = True, type=str, help="Girder file id to download.")
-    parser.add_argument("--directory","-d", action='store_true')
+    parser.add_argument("--directory","-d", action='store_true', help="Set this flag if the target is a directory")
     parser.add_argument("--output_dir", '-o', required = False, type=str, help="Path to the directory to download files to.", default="./tmp/")
     parser.add_argument("--filename", '-fn', required = False, type=str, default=None, help="Name to give to the file after downloading ")
     args = parser.parse_args()
