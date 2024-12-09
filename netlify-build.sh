930ff7171a44e8bc0f80fbe1f9953bdb4b049a67
Javier Cladellas <javiercladellas@gmail.com>
2024-12-09 10:36:57 +0100

    rm staging preview workflow (handled now by netlify) #181

--- netlify-build.sh
+++ netlify-build.sh
@@ -8,9 +8,13 @@ pip3 install -r requirements.txt
 pip3 wheel --no-deps --wheel-dir dist .
 pip3 install dist/*.whl
 npm i
-if [[ $BRANCH != *"new-benchmark"* ]]; then
+if [[ $BRANCH == *"new-benchmark"* ]]; then
     config_id=6752f0734c9ccbdde21a48ca
-    girder-download --girder_id=$config_id --output_dir=./tmp/ --filename=website_config.json
+    girder-download -gid $config_id -o ./tmp/ -d
+    merge-json-configs -fp ./tmp/**/website_config.json -o ./tmp/website_config.json -u
+else
+    config_id=6752f0734c9ccbdde21a48ca
+    girder-download -gid $config_id -o ./tmp/ -fn website_config.json
 fi
 render-benchmarks --config_file=./tmp/website_config.json
 npx antora --stacktrace generate --cache-dir cache --clean --html-url-extension-style=indexify site.yml
