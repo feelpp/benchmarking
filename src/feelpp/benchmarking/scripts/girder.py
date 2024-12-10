154a4167a89b577b9808347f343ddd6595106bff
Javier Cladellas <javiercladellas@gmail.com>
2024-12-10 09:27:46 +0100

    fix arg name

--- src/feelpp/benchmarking/scripts/girder.py
+++ src/feelpp/benchmarking/scripts/girder.py
@@ -26,6 +26,6 @@ def upload_cli():
     args = parser.parse_args()
 
     girder_handler = GirderHandler(download_base_dir=None)
-    girder_handler.upload( args.directory, args.girder_id )
+    girder_handler.upload( args.item, args.girder_id )
 
     return 0
