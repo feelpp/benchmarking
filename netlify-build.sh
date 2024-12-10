cacfba52b867711f3d2212f4aea41b7a8958eb56
Javier Cladellas <javiercladellas@gmail.com>
2024-12-10 12:29:50 +0100

    factor girder ids #181

--- netlify-build.sh
+++ netlify-build.sh
@@ -8,15 +8,14 @@ pip3 install -r requirements.txt
 pip3 wheel --no-deps --wheel-dir dist .
 pip3 install dist/*.whl
 npm i
+source ./girder_deploy_config.sh
 if [[ $HEAD == *"new-benchmark"* ]]; then
     echo "Downloading Staging benchmarks"
-    config_id=6752b5194c9ccbdde21a48b8
-    girder-download -gid $config_id -o ./tmp/ -d
+    girder-download -gid $staging_folder_id -o ./tmp/ -d
     merge-json-configs -fp ./tmp/**/website_config.json -o ./tmp/website_config.json -u
 else
     echo "Downloading Production benchmarks"
-    config_id=6752f0734c9ccbdde21a48ca
-    girder-download -gid $config_id -o ./tmp/ -fn website_config.json
+    girder-download -gid $production_website_config_id -o ./tmp/ -fn website_config.json
 fi
 render-benchmarks --config_file=./tmp/website_config.json
 npx antora --stacktrace generate --cache-dir cache --clean --html-url-extension-style=indexify site.yml
