e22ca9c408ca0f0889900d8c300209effbb77679
Javier Cladellas <javiercladellas@gmail.com>
2024-12-11 14:29:44 +0100

    add geomspace validation

--- src/feelpp/benchmarking/reframe/config/configParameters.py
+++ src/feelpp/benchmarking/reframe/config/configParameters.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, field_validator
 from typing import  Union, Optional, List, Any, Dict
 
 
@@ -9,7 +9,18 @@ class Linspace(BaseModel):
     n_steps:int
 
 class Geomspace(Linspace):
-    pass
+
+    @field_validator("min","max",mode="after")
+    @classmethod
+    def checkForZero(cls,v):
+        if v == 0:
+            raise ValueError("Geomspace cannot contain 0")
+        return v
+
+    @model_validator(mode="after")
+    def checkZeroInRange(self):
+        if (self.min < 0 and self.max > 0) or (self.max < 0 and self.min>0):
+            raise ValueError("0 cannot be contained between min and max")
 
 class Range(BaseModel):
     min:Union[float,int]
