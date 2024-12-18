3f7cfe7fe2bba6c1a3577ea1cef1c4e9e6f365dd
Javier Cladellas <javiercladellas@gmail.com>
2024-12-18 13:48:48 +0100

    change color and line mode #122

--- src/feelpp/benchmarking/report/figureFactory.py
+++ src/feelpp/benchmarking/report/figureFactory.py
@@ -44,7 +44,7 @@ class ScatterFigure(Figure):
             frame_traces = []
             for i,col in enumerate(self.fill_lines):
                 frame_traces.append(
-                    go.Scatter( x = frame_df.index, y = frame_df.loc[:,col], name = col, fill='tonexty' if i > 0 else None)
+                    go.Scatter( x = frame_df.index, y = frame_df.loc[:,col], name = col, fill='tonexty' if i > 0 else None, line=dict(color="black",dash="dash") ,mode="lines")
                 )
 
             for col in [c for c in frame_df.columns if c not in self.fill_lines]:
@@ -100,7 +100,7 @@ class ScatterFigure(Figure):
         )
         for i,col in enumerate(self.fill_lines):
             figure.add_trace(
-                go.Scatter( x = df.index, y = df.loc[:,col], name = col, fill='tonexty' if i < len(self.fill_lines) else None)
+                go.Scatter( x = df.index, y = df.loc[:,col], name = col, fill='tonexty' if i < len(self.fill_lines) else None, line=dict(color="black",dash="dash") ,mode="lines")
             )
 
         for col in [c for c in df.columns if c not in self.fill_lines]:
