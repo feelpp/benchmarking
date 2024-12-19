36d319c2733ec5ae1123e58dc1ebc91de19479f4
Javier Cladellas <javiercladellas@gmail.com>
2024-12-19 09:39:02 +0100

    change patches ids separator ("-" to ":")

--- src/feelpp/benchmarking/report/parser.py
+++ src/feelpp/benchmarking/report/parser.py
@@ -20,7 +20,7 @@ class ReportArgParser():
         self.parser.add_argument("--modules-path", "-m", type=str, help="Path to the modules directory where reports will be rendered", default="./docs/modules/ROOT/pages")
         self.parser.add_argument("--overview-config", "-oc", type=str, help="Path to the overview configuration file", default="./src/feelpp/benchmarking/report/config/overviewConfig.json"),
         self.parser.add_argument("--plot-configs", "-pc", type=str, nargs='+',default=[], action='extend', help="Path the a plot configuration to use for a given benchmark. To be used along with --patch-reports")
-        self.parser.add_argument("--patch-reports","-pr", type=str, nargs='+',default=[], action='extend', help="Id of the reports to path, the syntax of the id is machine-application-usecase-date e.g. gaya-feelpp_app-my_use_case-2024_11_05T01_05_32. It is possible to affect all reports in a component by replacing the machine, application, use_case or date by 'all'. Also, one can indicate to patch the latest report by replacing the date by 'latest'. If this option is not provided but plot-configs is, then the latest report will be patched (most recent report date)")
+        self.parser.add_argument("--patch-reports","-pr", type=str, nargs='+',default=[], action='extend', help="Id of the reports to path, the syntax of the id is machine:application:usecase:date e.g. gaya:feelpp_app:my_use_case:2024_11_05T01_05_32. It is possible to affect all reports in a component by replacing the machine, application, use_case or date by 'all'. Also, one can indicate to patch the latest report by replacing the date by 'latest'. If this option is not provided but plot-configs is, then the latest report will be patched (most recent report date)")
         self.parser.add_argument("--save-patches","-sp", action='store_true', help="If this flag is active, existing plot configurations will be replaced with the ones provided in patch-reports.")
         self.parser.add_argument("--website","-w", action='store_true', help="Compile documentation and start HTTP server with benchmark reports")
 
@@ -30,7 +30,7 @@ class ReportArgParser():
 
         if self.args.patch_reports:
             for patch_report in self.args.patch_reports:
-                splitted_patch = patch_report.split("-")
+                splitted_patch = patch_report.split(":")
                 if len(splitted_patch) != 4:
                     raise ValueError(f"The ID syntaxt is incorrect ({patch_report})")
                 machine, app, use_case, date = splitted_patch
@@ -39,7 +39,7 @@ class ReportArgParser():
                 if machine == "all":
                     raise ValueError("The machine component patch does not support the 'all' keyworkd")
 
-            self.args.patch_reports = [patch_report.split("-") for patch_report in self.args.patch_reports]
+            self.args.patch_reports = [patch_report.split(":") for patch_report in self.args.patch_reports]
 
     def checkDirectoriesExist(self):
         """ Check that directories passed as arguments exist in the filesystem"""
