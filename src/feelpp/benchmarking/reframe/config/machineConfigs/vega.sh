d78e405c3132b567a3683b36ffd2c5807828f134
Javier Cladellas <javiercladellas@gmail.com>
2024-12-20 13:43:33 +0100

    try using python module #206

--- src/feelpp/benchmarking/reframe/config/machineConfigs/vega.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/vega.sh
@@ -3,7 +3,10 @@
 # module load Python/3.12.3-GCCcore-13.3.0
 
 
-/cvmfs/sling.si/modules/el7/software/Python/3.12.3-GCCcore-13.3.0/bin/python -m venv .venv
+# /cvmfs/sling.si/modules/el7/software/Python/3.12.3-GCCcore-13.3.0/bin/python -m venv .venv
+
+module load Python/3.12.3-GCCcore-13.3.0
+python3 -m venv .venv
 source .venv/bin/activate
-.venv/bin/python3.12 -m pip install --upgrade pip
-.venv/bin/python3.12 -m pip install -r requirements.txt
+.venv/bin/python3 -m pip install --upgrade pip
+.venv/bin/python3 -m pip install -r requirements.txt
