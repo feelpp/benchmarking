f8af9bc718b5510527533d28fbe0243ac645fc34
Javier Cladellas <javiercladellas@gmail.com>
2024-12-20 11:33:14 +0100

    init vega rfm cfg #206

---
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/vega.py
@@ -0,0 +1,50 @@
+import socket
+
+hostname = socket.gethostname()
+print("Hostname:", hostname)
+
+site_configuration = {
+    'systems':[
+        {
+            'name': 'vega',
+            'descr': 'Vega',
+            'hostnames': [f'{hostname}'],
+            'modules_system': 'tmod4',
+            'partitions': [
+                {
+                    'name': 'cpu',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    # 'max_jobs': 8,
+                    'access': [f"--partition=cpu"],
+                    'environs': ['default'],
+                    'processor': {
+                        'num_cpus': 128
+                    },
+                    'devices': [
+                        {
+                            'type': 'cpu',
+                            'num_devices': 960
+                        }
+                    ],
+                    'container_platforms':[
+                        {
+                            'type': 'Singularity'
+                        }
+                    ],
+                    'sched_options': { 'use_nodes_option': True },
+                }
+            ],
+            'env_vars':[
+                ["OMP_NUM_THREADS",1]
+            ]
+        }
+    ],
+    'environments': [
+        {
+            'name':'default',
+            'modules': [],  #["OpenMPI/4.1.5-GCC-12.3.0"],
+            'target_systems':['vega:cpu']
+        }
+    ]
+}
