4efc5e39a8f427cd62d2db9c0a2562dbff2363e4
Javier Cladellas <javiercladellas@gmail.com>
2025-01-13 10:45:18 +0100

    explicit python version #206 [ci skip]

--- src/feelpp/benchmarking/reframe/config/machineConfigs/vega.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/vega.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -l
 
-module load Python/3.10.8-GCCcore-12.2.0
-python3 -m venv .venv
+# module load Python/3.10.8-GCCcore-12.2.0
+/cvmfs/sling.si/modules/el7/software/Python/3.10.8-GCCcore-12.2.0/bin/python3.10 -m venv .venv
 source .venv/bin/activate
-.venv/bin/python3 -m pip install --upgrade pip
-.venv/bin/python3 -m pip install -r requirements.txt
-ldd .venv/bin/python3
+.venv/bin/python3.10 -m pip install --upgrade pip
+.venv/bin/python3.10 -m pip install -r requirements.txt
