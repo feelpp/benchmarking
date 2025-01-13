14262e9fe99f808549ab7a670b80fb5080ba9ab0
Javier Cladellas <javiercladellas@gmail.com>
2025-01-13 09:01:14 +0100

    rm comments #206

--- src/feelpp/benchmarking/reframe/config/machineConfigs/vega.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/vega.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -l
 
-# module load Python/3.12.3-GCCcore-13.3.0
-
-
-# /cvmfs/sling.si/modules/el7/software/Python/3.12.3-GCCcore-13.3.0/bin/python -m venv .venv
-
 module load Python/3.12.3-GCCcore-13.3.0
 python3 -m venv .venv
 source .venv/bin/activate
