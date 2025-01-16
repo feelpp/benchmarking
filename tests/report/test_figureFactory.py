649b8168fb6810edce98f3fd1c538573f7f8b1f8
Javier Cladellas <javiercladellas@gmail.com>
2025-01-16 15:24:43 +0100

    rename test (strategies)

--- tests/report/test_figureFactory.py
+++ tests/report/test_figureFactory.py
@@ -1,5 +1,5 @@
 from feelpp.benchmarking.report.figureFactory import FigureFactory, ScatterFigure, TableFigure, StackedBarFigure, GroupedBarFigure
-from test_strategies import PlotConfigMocker
+from test_transformationStrategies import PlotConfigMocker
 import pytest
 
 
