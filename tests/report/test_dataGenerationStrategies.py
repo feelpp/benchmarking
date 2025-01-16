034f6dc0126033c8a9beab3571551cdbb81f2847
Javier Cladellas <javiercladellas@gmail.com>
2025-01-16 17:28:54 +0100

    add dataGenerationStrategies tests #170

--- tests/report/test_dataGenerationStrategies.py
+++ tests/report/test_dataGenerationStrategies.py
@@ -1,18 +1,73 @@
 import pytest
+from feelpp.benchmarking.report.figureFactory import FigureFactory
+from feelpp.benchmarking.report.transformationStrategies import TransformationStrategyFactory
 from feelpp.benchmarking.report.dataGenerationStrategies import DataGeneratorFactory, PlotlyGenerator, PlotlyHtmlGenerator, CsvGenerator, PgfGenerator
-from test_transformationStrategies import PlotConfigMocker
+from test_transformationStrategies import PlotConfigMocker, AxisMocker, MockDataframe
+import pandas as pd
+import numpy as np
 
-class TestPlotlyGenerator:
-    pass
+class TestDataGeneratorStrategies:
+    """ Tests all DataGenerator strategies. Compares the computed result of the strategy methods against the expected result"""
 
-class TestPlotlyHtmlGenerator:
-    pass
+    plot_config = PlotConfigMocker(
+        xaxis=AxisMocker(parameter="xaxis",label="x"),
+        secondary_axis=AxisMocker(parameter="secondary_axis",label="secondary"),
+        color_axis=AxisMocker(parameter="color_axis",label="color"),
+        transformation="performance",
+        plot_types=["scatter","stacked_bar"]
+    )
+    mock_data = MockDataframe("multi").df
 
-class TestPgfGenerator:
-    pass
+    def getData(self,generator):
+        """ Calls the generate method of a given strategy (generator), performs basic common tests and returns the data"""
+        data = generator.generate([self.plot_config], self.mock_data)
+        assert isinstance(data,list)
+        assert len(data) == len(self.plot_config.plot_types), "Different number of generated data and plot_types "
+        return data
 
-class TestCsvGenerator:
-    pass
+    def test_plotlyGenerator(self):
+        """ Tests for the PlotlyGenerator strategy.
+        Checks that figures are identical as expected ones
+        """
+        generator = PlotlyGenerator()
+        figures = self.getData(generator)
+
+        expected_figures = [plot.createFigure(self.mock_data) for plot in FigureFactory.create(self.plot_config)]
+
+        assert figures == expected_figures, "Figures are not the same as returned by the FigureFactory"
+
+    def test_plotlyHtmlGenerator(self):
+        """ Tests for the plotlyHtmlGenerator strategy.
+        - Checks that figures start and end with an html tag
+        - Checks that html strings have expected lenghts
+        """
+        generator = PlotlyHtmlGenerator()
+        figures = self.getData(generator)
+        expected_figures = [plot.createFigure(self.mock_data).to_html() for plot in FigureFactory.create(self.plot_config)]
+
+        assert all([fig.startswith("<html>") and fig.endswith("</html>") for fig in figures]), "Figures do not start or end with html tags"
+        assert all([len(fig) == len(expected_fig) for fig,expected_fig in zip(figures,expected_figures)]), "Figures html does not have the expected length"
+
+    def test_csvGenerator(self):
+        """Tests for the csvGenerator strategy"""
+        generator = CsvGenerator()
+        data = self.getData(generator)
+        expected_data = [TransformationStrategyFactory.create(self.plot_config).calculate(self.mock_data).to_csv() for _ in self.plot_config.plot_types]
+
+        assert all([d == expected  for d,expected in zip(data,expected_data) ])
+
+    def test_pgfGenerator(self):
+        """ Tests for the pgfGenerator strategy"""
+        generator = PgfGenerator()
+
+        old_types = self.plot_config.plot_types
+        self.plot_config.plot_types = ["scatter","scatter"] #At the moment just scatter is implemented for pgf
+
+        figures = self.getData(generator)
+
+        pass #TODO: Currently not tested until tikzplotly is more robust.
+
+        self.plot_config.plot_types = old_types
 
 
 
