dd06114de67abd84c6dfb83c932afa4a44457283
Javier Cladellas <javiercladellas@gmail.com>
2025-02-07 13:51:08 +0100

    accept arbitrary fields (for refactoring config )
    Oopsie, thought this was default behaviour :(

--- src/feelpp/benchmarking/reframe/config/configSchemas.py
+++ src/feelpp/benchmarking/reframe/config/configSchemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, field_validator, model_validator, RootModel
+from pydantic import BaseModel, field_validator, model_validator, RootModel, ConfigDict
 from typing import Literal, Union, Optional, List, Dict
 from feelpp.benchmarking.reframe.config.configParameters import Parameter
 from feelpp.benchmarking.reframe.config.configPlots import Plot
@@ -113,6 +113,13 @@ class ConfigFile(BaseModel):
     parameters: List[Parameter]
     additional_files: Optional[AdditionalFiles] = None
     plots: Optional[List[Plot]] = []
+
+    model_config = ConfigDict( extra='allow' )
+    def __getattr__(self, item):
+        if item in self.model_extra:
+            return self.model_extra[item]
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")
+
 
     @field_validator("timeout",mode="before")
     @classmethod
