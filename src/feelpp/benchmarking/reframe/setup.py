98ebf270efc7ce6b9a4cdd5b9f0835bd40d3b6e6
Javier Cladellas <javiercladellas@gmail.com>
2025-02-12 09:09:02 +0100

    fix code injection vulnerability (finally) #164

--- src/feelpp/benchmarking/reframe/setup.py
+++ src/feelpp/benchmarking/reframe/setup.py
@@ -60,7 +60,7 @@ class ReframeSetup(rfm.RunOnlyRegressionTest):
 
     parameter_handler = ParameterHandler(app_reader.config.parameters)
     for param_name,param_values in parameter_handler.parameters.items():
-        exec(f"{param_name}=parameter({param_values})")
+        locals()[param_name]=parameter(param_values)
 
 
     @run_after('init')
