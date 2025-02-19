299aa925b318063b0c0637c24242b43312172155
Javier Cladellas <javiercladellas@gmail.com>
2025-02-19 17:25:05 +0100

    try meluxina sumbit from comput node #236

---
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/launchBenchmark.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --qos=${QOS}
+#SBATCH --ntasks-per-node=8         #reframe can submit 8 jobs at the time, async.
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --partition=${SUBMIT_PARTITION}
+#SBATCH --account=${ACCOUNT}
+
+
+export GIRDER_API_KEY=$GIRDER_API_KEY
+
+module load Python/3.8.6-GCCcore-10.2.0
+python3.8 -m venv .venv
+source .venv/bin/activate
+.venv/bin/python3.8 -m pip install --upgrade pip
+.venv/bin/python3.8 -m pip install -r requirements.txt
+
+while getopts "m:b:p:" opt; do
+  case $opt in
+    m) MACHINE_CONFIG=$OPTARG ;;
+    b) BENCHMARK_CONFIG=$OPTARG ;;
+    p) PLOTS_CONFIG=$OPTARG ;;
+    *) echo "Usage: $0 [-m machine_config] [-b benchmark_config] [-p plots_config]" ;;
+  esac
+done
+
+
+execute-benchmark \
+    -mc $MACHINE_CONFIG \
+    -bc $BENCHMARK_CONFIG \
+    -pc $PLOTS_CONFIG \
+    -v
--- src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/setup.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/setup.sh
@@ -4,8 +4,3 @@
 echo "==========================="
 myquota
 echo "==========================="
-
-python3.8 -m venv .venv
-source .venv/bin/activate
-.venv/bin/python3.8 -m pip install --upgrade pip
-.venv/bin/python3.8 -m pip install -r requirements.txt
