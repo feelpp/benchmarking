703a42aada0094e0d73c3fa1a84614373fb82b61
Javier Cladellas <javiercladellas@gmail.com>
2025-02-19 16:27:38 +0100

    try python3 -> python3.8 [ci skip]

--- src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/setup.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/setup.sh
@@ -5,7 +5,7 @@ echo "==========================="
 myquota
 echo "==========================="
 
-python3 -m venv .venv
+python3.8 -m venv .venv
 source .venv/bin/activate
-.venv/bin/python3 -m pip install --upgrade pip
-.venv/bin/python3 -m pip install -r requirements.txt
+.venv/bin/python3.8 -m pip install --upgrade pip
+.venv/bin/python3.8 -m pip install -r requirements.txt
