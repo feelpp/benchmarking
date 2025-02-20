c10480fa9eeae6cff77639bb708a727507e8e3a7
Javier Cladellas <javiercladellas@gmail.com>
2025-02-20 14:38:38 +0100

    factor js and hande single file download #226

--- src/feelpp/benchmarking/report/templates/js/figureHelpers.js
+++ src/feelpp/benchmarking/report/templates/js/figureHelpers.js
@@ -1,24 +1,45 @@
+function downloadData(data, filename) {
+    if (data.length == 1) {
+        if (data[0].content.length == 1) {
+            d = data[0].content[0];
+            const blob = new Blob([d.data], { type: "plain/text;charset=utf-8" });
+            downloadBlob(blob, `${d.title}.${data[0].format}`);
+        }
+        else {
+            downloadFilesAsZip(data[0].content, filename);
+        }
+    }
+    else if (data.length > 1) {
+        downloadFilesAsZip(data, filename);
+    }
+    else {
+        alert("No data to download");
+    }
+}
+
+
 function downloadFilesAsZip(data, zipFilename) {
     const zip = new JSZip();
-
     data.forEach((d) => {
         d.content.forEach((c) => {
-        const filename = `${c.title}.${d.format}`;
-        zip.file(filename, c.data);
+            const filename = `${c.title}.${d.format}`;
+            zip.file(filename, c.data);
         });
     });
-
-    zip.generateAsync({ type: "blob" }).then((blob) => {
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = zipFilename || "download.zip";
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-    });
+    zip.generateAsync({ type: "blob" }).then((blob) => downloadBlob(blob, zipFilename));
 }
+
+function downloadBlob(blob, filename) {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
 
 
 function switchTab(figureIndex, tabIndex) {
