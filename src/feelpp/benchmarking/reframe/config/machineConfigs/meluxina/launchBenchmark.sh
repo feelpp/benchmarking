a12d0bd9d8e8b1586b6cc4d2bb730d90f22653c5
Javier Cladellas <javiercladellas@gmail.com>
2025-03-14 08:46:37 +0100

    load default python module for meluxina [ci skip]

--- src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/launchBenchmark.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/meluxina/launchBenchmark.sh
@@ -5,11 +5,11 @@ module load Apptainer/1.2.4-GCCcore-12.3.0
 
 export GIRDER_API_KEY=$GIRDER_API_KEY
 
-module load Python/3.8.6-GCCcore-10.2.0
-python3.8 -m venv .venv
+module load Python
+python -m venv .venv
 source .venv/bin/activate
-.venv/bin/python3.8 -m pip install --upgrade pip
-.venv/bin/python3.8 -m pip install -r requirements.txt
+.venv/bin/python -m pip install --upgrade pip
+.venv/bin/python -m pip install -r requirements.txt
 
 while getopts "m:b:p:" opt; do
   case $opt in
