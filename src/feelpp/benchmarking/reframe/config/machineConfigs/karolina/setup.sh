270391ada89935268977f45b2dca2d1110f5df03
Javier Cladellas <javiercladellas@gmail.com>
2025-03-14 09:08:06 +0100

    try fix python loading for karolina [ci skip]

--- src/feelpp/benchmarking/reframe/config/machineConfigs/karolina/setup.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/karolina/setup.sh
@@ -6,8 +6,7 @@ it4ifsusage
 echo "==========================="
 
 
-module load Python/3.10.4-GCCcore-11.3.0
-python3.10 -m venv .venv
+/apps/all/Python/3.10.4-GCCcore-11.3.0/bin/python -m venv .venv
 source .venv/bin/activate
 .venv/bin/python3.10 -m pip install --upgrade pip
 .venv/bin/python3.10 -m pip install -r requirements.txt
