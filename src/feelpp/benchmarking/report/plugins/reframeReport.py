08d1e0772714674402c8c798138a5bc37f32abad
Javier Cladellas <javiercladellas@gmail.com>
2025-10-28 17:14:49 +0100

    up rfm table plugin  with latest v

--- src/feelpp/benchmarking/report/plugins/reframeReport.py
+++ src/feelpp/benchmarking/report/plugins/reframeReport.py
@@ -12,12 +12,14 @@ class ReframeReport:
             run_df = run_df.rename(columns={c:f"runs.{c}" for c in run_df.columns})
             testcases = []
             for testcase in run["testcases"]:
-                perfvar_df =  pd.DataFrame.from_dict(testcase["perfvars"])
-                perfvar_df = perfvar_df.rename(columns={c:f"perfvars.{c}" for c in perfvar_df})
+                perfvar_df =  pd.DataFrame.from_dict(testcase["perfvalues"])
+                perfvar_df = perfvar_df.iloc[:1,:]
+                perfvar_df.rename(columns={c:c.split(":")[-1] for c in perfvar_df.columns},inplace=True)
+                perfvar_df = perfvar_df.rename(columns={c:f"perfvalues.{c}" for c in perfvar_df})
 
-                testcase_df = pd.DataFrame.from_dict(testcase,orient="index").T.drop(columns=["perfvars","check_params","check_vars"])
+
+                testcase_df = pd.DataFrame.from_dict(testcase,orient="index").T.drop(columns=["perfvalues","check_params"]+list(testcase["check_params"].keys()))
                 testcase_df = testcase_df.rename(columns={c:f"testcases.{c}" for c in testcase_df})
-
                 param_dict = {}
                 for dim, v in testcase["check_params"].items():
                     if isinstance(v,dict):
@@ -26,10 +28,8 @@ class ReframeReport:
                     else:
                         param_dict[dim] = v
                 param_df = pd.DataFrame.from_dict(param_dict,orient="index").T
-                var_df = pd.DataFrame(index=testcase["check_vars"].keys(),data=[str(v) for v in testcase["check_vars"].values()]).T
-                var_df = var_df.rename(columns={c:f"check_vars.{c}" for c in var_df})
 
-                testcase_df = pd.concat([testcase_df,param_df,var_df],axis=1)
+                testcase_df = pd.concat([testcase_df,param_df],axis=1)
                 testcases.append(pd.concat([perfvar_df,testcase_df.loc[testcase_df.index.repeat(perfvar_df.shape[0])].reset_index(drop=True)],axis=1))
 
             testcases_df = pd.concat(testcases,axis=0)
