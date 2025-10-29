447632f2c457281bcf25df058d2eda2d2096adc6
Javier Cladellas <javiercladellas@gmail.com>
2025-10-29 09:57:59 +0100

    handle empty case

--- src/feelpp/benchmarking/report/plugins/reframeReport.py
+++ src/feelpp/benchmarking/report/plugins/reframeReport.py
@@ -12,10 +12,12 @@ class ReframeReport:
             run_df = run_df.rename(columns={c:f"runs.{c}" for c in run_df.columns})
             testcases = []
             for testcase in run["testcases"]:
-                perfvar_df =  pd.DataFrame.from_dict(testcase["perfvalues"])
-                perfvar_df = perfvar_df.rename(index={0:"value",1:"reference",2:"low_threshold",3:"up_threshold",4:"unit",5:"result"})
-                perfvar_df = perfvar_df.T.reset_index().rename(columns={"index":"perfvalue"})
-                perfvar_df[["system","partition","perfvalue"]] = perfvar_df["perfvalue"].str.split(":",expand=True)
+                perfvar_df = pd.DataFrame()
+                if "perfvalues" in testcase and testcase["perfvalues"] and testcase["perfvalues"] != {}:
+                    perfvar_df =  pd.DataFrame.from_dict(testcase["perfvalues"])
+                    perfvar_df = perfvar_df.rename(index={0:"value",1:"reference",2:"low_threshold",3:"up_threshold",4:"unit",5:"result"})
+                    perfvar_df = perfvar_df.T.reset_index().rename(columns={"index":"perfvalue"})
+                    perfvar_df[["system","partition","perfvalue"]] = perfvar_df["perfvalue"].str.split(":",expand=True)
                 testcase_df = pd.DataFrame.from_dict(testcase,orient="index").T.drop(columns=["perfvalues","check_params"]+list(testcase["check_params"].keys()))
                 testcase_df = testcase_df.rename(columns={c:f"testcases.{c}" for c in testcase_df})
                 param_dict = {}
