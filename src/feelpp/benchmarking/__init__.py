f05ba80a172e34c1ebc3f11e940d55a377ba4aa5
Javier Cladellas <javiercladellas@gmail.com>
2025-11-14 15:12:54 +0100

    add version

--- src/feelpp/benchmarking/__init__.py
+++ src/feelpp/benchmarking/__init__.py
@@ -1,5 +1,6 @@
+from importlib_metadata import version, PackageNotFoundError
 
-
-
-
-
+try:
+    __version__ = version("feelpp.benchmarking")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
