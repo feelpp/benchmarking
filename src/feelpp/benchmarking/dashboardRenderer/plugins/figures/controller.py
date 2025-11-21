be756368e6a6c1c27556b9364b02d0f5ea25522b
Javier Cladellas <javiercladellas@gmail.com>
2025-11-21 16:00:40 +0100

    accept dict figs

--- src/feelpp/benchmarking/dashboardRenderer/plugins/figures/controller.py
+++ src/feelpp/benchmarking/dashboardRenderer/plugins/figures/controller.py
@@ -1,20 +1,31 @@
 from feelpp.benchmarking.dashboardRenderer.plugins.figures.figureFactory import FigureFactory
 from feelpp.benchmarking.dashboardRenderer.plugins.figures.schemas.plot import Plot
+from typing import List, Dict, Union
 
 class Controller:
     """ Controller component , it orchestrates the model with the view"""
-    def __init__(self, df, plots_config):
+    def __init__(self, df, plots_config: Union[List[Dict],Dict,Plot,List[Plot]]):
         """
         Args:
             model (pd.DataFrame): The atomic report model component
             view (AtomicReportView): The atomic report view component
         """
         self.df = df
-        self.plots_config = [Plot(**d) for d in plots_config]
+        if isinstance(plots_config,Plot):
+            self.plots_config = [plots_config]
+        elif isinstance(plots_config,Dict):
+            self.plots_config = [Plot(**plots_config)]
+        elif isinstance(plots_config,List):
+            if all(isinstance(d,Plot) for d in plots_config):
+                self.plots_config = plots_config
+            else:
+                self.plots_config = [Plot(**d) for d in plots_config]
+        else:
+            raise TypeError(f"plots_config must be a Dict or List of Dicts, got {type(plots_config)}")
         self.figures = [FigureFactory.create(plot_config) for plot_config in self.plots_config]
 
     def generateAll(self):
-        if self.df.empty:
+        if self.df is None or self.df.empty:
             return []
         return [
             self.generateFigure(figure,plot_config.plot_types)
