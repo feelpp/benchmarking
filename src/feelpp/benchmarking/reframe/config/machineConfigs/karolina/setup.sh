ecbc25593718f5f921daa3e8c399291a78425d0d
Javier Cladellas <javiercladellas@gmail.com>
2025-11-25 15:57:19 +0100

    use uv #302 and hotfix for pr name We'll try to let the ci handle installations. Might not work on euroHPC systems!!!

--- src/feelpp/benchmarking/reframe/config/machineConfigs/karolina/setup.sh
+++ src/feelpp/benchmarking/reframe/config/machineConfigs/karolina/setup.sh
@@ -6,10 +6,10 @@ it4ifsusage
 echo "==========================="
 
 
-module load Python/3.10.4-GCCcore-11.3.0
-export LD_LIBRARY_PATH=/apps/all/Python/3.10.4-GCCcore-11.3.0/lib:$LD_LIBRARY_PATH
-python3.10 -m venv .venv
-echo 'export LD_LIBRARY_PATH=/apps/all/Python/3.10.4-GCCcore-11.3.0/lib:$LD_LIBRARY_PATH' >> .venv/bin/activate
-source .venv/bin/activate
-.venv/bin/python3.10 -m pip install --upgrade pip
-.venv/bin/python3.10 -m pip install -r requirements.txt
+# module load Python/3.10.4-GCCcore-11.3.0
+# export LD_LIBRARY_PATH=/apps/all/Python/3.10.4-GCCcore-11.3.0/lib:$LD_LIBRARY_PATH
+# python3.10 -m venv .venv
+# echo 'export LD_LIBRARY_PATH=/apps/all/Python/3.10.4-GCCcore-11.3.0/lib:$LD_LIBRARY_PATH' >> .venv/bin/activate
+# source .venv/bin/activate
+# .venv/bin/python3.10 -m pip install --upgrade pip
+# .venv/bin/python3.10 -m pip install -r requirements.txt
