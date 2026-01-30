b612a79374caaf0552ab2824f4ea1c21f3d62520
Javier Cladellas <javiercladellas@gmail.com>
2026-01-30 16:48:49 +0100

    fix tests

--- src/feelpp/benchmarking/json_report/tests/test_jsonReport.py
+++ src/feelpp/benchmarking/json_report/tests/test_jsonReport.py
@@ -8,20 +8,21 @@ def normalizeReport(content: str) -> str:
     content = re.sub( r"^:docdatetime: .*?$", ":docdatetime: <TIMESTAMP>", content, flags=re.MULTILINE )
 
     content = re.sub(
-        r"<button[^>]*?>\s*(.*?)\s*</button>",
-        lambda match : f"<button>{match.group(1).strip()}</button>",
+        r"graph-div-.*?-",
+        "<FIGURE_ID>",
         content, flags=re.DOTALL
     )
     content = re.sub(
-        r"<div[^>]*>\s*<div[^>]*></div>\s*<script.*?</script>\s*</div>",
-        """
-        <div>
-            <div>FIGURE</div>
-            <script></script>
-        </div>""",
-        content,
-        flags=re.DOTALL
+        r"<a href='.*?' download>CSV</a>",
+        "<ZIPPATH>",
+        content, flags=re.DOTALL
     )
+    content = re.sub(
+        r"fetch\('.*?'\)",
+        "<JSONPATH>",
+        content, flags=re.DOTALL
+    )
+
 
     lines = [line.rstrip() for line in content.splitlines() if line.strip()]
     return "\n".join(lines)
@@ -54,6 +55,6 @@ def test_jsonReportCases(report_filename,output_format="adoc"):
     )
 
     with tempfile.TemporaryDirectory() as tmpdir:
-        output_file = controller.render(output_dirpath=os.path.join(data_dir,"outputs"))
+        output_file = controller.render(output_dirpath=os.path.join(data_dir,tmpdir))
         golden_file = os.path.join(data_dir,"golden",report_filename.replace(".json",f".{output_format}"))
         assert_report_matches_golden(output_file,golden_file)
